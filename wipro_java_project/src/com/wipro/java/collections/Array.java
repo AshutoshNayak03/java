160e98529bd794c3270b4e434d7bcad2ff8ba568
Ashutosh Nayak <ashutoshnayak.official@gmail.com>
2025-02-05 14:14:37 +0530

    day 4

---
+++ wipro_java_project/src/com/wipro/java/collections/Array.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.java.collections;
+
+/**
+ * 
+ */
+public class Array {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr;
+		arr=new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=20;
+		arr[3]=20;
+		arr[4]=20;
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("element at index"+i+":"+arr[i]);
+		}
+	}
+
+}
